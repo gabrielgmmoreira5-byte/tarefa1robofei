a58d6a90c441b40780ebaf221472df769aa69ef0
Gabrie Moreira <gabrielgmmoreira5@gmail.com>
2025-09-30 23:46:38 -0300

    Update testepublisher.cpp

--- tarefa1_robf/src/testepublisher.cpp
+++ tarefa1_robf/src/testepublisher.cpp
@@ -5,19 +5,18 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+//aqui em cima são algumas dependências
 
-using namespace std::chrono_literals;
+using namespace std::chrono_literals; //nos permite escrever os tempos de forma mais legível
 
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
 
-class Publicadorobo : public rclcpp::Node
+class Publicadorobo : public rclcpp::Node //cria a classe do nó
 {
 public:
   Publicadorobo()
-  : Node("publiquinha"), count_(0)
+  : Node("publiquinha"), count_(0) //aqui é o nome do nó
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10); //cria um publisher que manda mensagens string no tópico "topic"
     timer_ = this->create_wall_timer(
       500ms, std::bind(&Publicadorobo::timer_callback, this));
   }
@@ -26,19 +25,19 @@ private:
   void timer_callback()
   {
     auto message = std_msgs::msg::String();
-    message.data = "Vambora " + std::to_string(count_++);
+    message.data = "Vambora " + std::to_string(count_++); //cria a mensagem e publica no tópico
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_; //atributos privados
   size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<Publicadorobo>());
+  rclcpp::spin(std::make_shared<Publicadorobo>()); //no main se inicializa, roda e encerra o sistema ros2
   rclcpp::shutdown();
   return 0;
 }
